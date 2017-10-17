b64e326d68d97d75ad5156e5957748cea54e0141
SylvainLan <sylvain.lannuzel@student.ecp.fr>
2017-10-17 14:11:19 -0400

    network is learning, todo : add batch size

--- models/fcnn.py
+++ models/fcnn.py
@@ -8,18 +8,16 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Net(nn.Module):
+    def __init__(self, d_in, inner_layer_size=[50, 50], d_out=2):
+        super(Net, self).__init__()
+        self.input_layer = nn.Linear(d_in, inner_layer_size[0])
+        self.hidden_layer = nn.Linear(inner_layer_size[0], inner_layer_size[1])
+        self.output_layer = nn.Linear(inner_layer_size[1], d_out)
 
-	def __init__(self, D_in, inner_layer_size = [50,50], D_out=2):
-		super(Net,self).__init__()
-		self.input_layer = nn.Linear(D_in, inner_layer_size[0])
-		self.hidden_layer = nn.Linear(inner_layer_size[0], inner_layer_size[1])
-		self.output_layer = nn.Linear(inner_layer_size[1], D_out)
-		
-
-	def forward(self,x):
-		x = F.relu(self.input_layer(x))
-		x = F.relu(self.hidden_layer(x))
-		x = F.relu(self.output_layer(x))
-		return x
-
+    def forward(self, x):
+        x = F.relu(self.input_layer(x))
+        x = F.relu(self.hidden_layer(x))
+        x = F.relu(self.output_layer(x))
+        return x
