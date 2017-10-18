3af0bdcf2f3e7e7b5a3fd67f454569a2c7287cb7
SylvainLan <sylvain.lannuzel@student.ecp.fr>
2017-10-18 14:18:06 -0400

    no visualization

--- cifar10_tutorial.py
+++ cifar10_tutorial.py
@@ -83,26 +83,26 @@ classes = ('plane', 'car', 'bird', 'cat',
 ########################################################################
 # Let us show some of the training images, for fun.
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-# functions to show an image
-
-
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
-
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# # functions to show an image
+#
+#
+# def imshow(img):
+#     img = img / 2 + 0.5     # unnormalize
+#     npimg = img.numpy()
+#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+#
+#
+# # get some random training images
+# dataiter = iter(trainloader)
+# images, labels = dataiter.next()
+#
+# # show images
+# imshow(torchvision.utils.make_grid(images))
+# # print labels
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 
 ########################################################################
