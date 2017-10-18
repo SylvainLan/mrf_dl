00295720795270ca269eb5c26a2f6952fb54ff28
SylvainLan <sylvain.lannuzel@student.ecp.fr>
2017-10-18 13:57:33 -0400

    small fixes

---
+++ train_lstm.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import torch
+from torch.autograd import Variable
+from models.gflstm import GFLSTM
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from data.mag_data_load import mag_load
+
+b_size = 4
+
+trainset = mag_load('./data/T1_class', train = True, download = True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size = b_size, shuffle = True)
+testset = mag_load('./data/T1_class', train = False, download = True)
+testloader = torch.utils.data.DataLoader(testset, batch_size = 1, shuffle = False)
+
+gf = GFLSTM(850, 100, 2)
+lastL = nn.Linear(1000, 44)
+criterion = nn.MSELoss()
+optimizer = optim.SGD(gf.parameters(),lr = 0.01, momentum = 0.9)
+gf.cuda()
