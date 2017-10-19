a47edbca652ea25cfe3a6a71b3f418ed364c2549
SylvainLan <sylvain.lannuzel@student.ecp.fr>
2017-10-19 17:40:10 -0400

    change in the call of sbatch

--- test.sh
+++ test.sh
@@ -3,15 +3,10 @@
 #SBATCH --job-name=0_python3--test_cuda.py
 ript-validate.sh-pronoun_enfr_dev.lang1-pronoun_enfr_dev.lang2
 #SBATCH --gres=gpu:k80:1
-#SBATCH --time=00:10:00
-#SBATCH --mem=32GB
+#SBATCH --time=01:00:00
+#SBATCH --mem=2GB
 #SBATCH --nodes=1
-#SBATCH --error=/home/ss11642/mrf_dl
-save_model_trained_-proj_share_weight_-external_validation_script_validate.sh_pronoun_enfr_dev.lang1_pronoun_enfr_dev.lang2/slurm_%j.err
-#SBATCH --output=/home/ss11642/mrf_dl
--save_model_trained_-proj_share_weight_-external_validation_script_validate.sh_pronoun_enfr_dev.lang1_pronoun_enfr_dev.lang2/slurm_%j.out
-
-
+#SBATCH --output=/home/ss11642/mrf_dl/slurm_%j.out
 
 module purge
 module load python3/intel/3.5.3
@@ -22,4 +17,6 @@ SRC=/home/ss11642/mrf_dl
 
 
 
-cd $SRC ; python3 test_cuda.py
+cd $SRC
+
+python3 test_cuda.py
