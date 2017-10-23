174b363cc9c7018ef65af8c3e2849dafd5e75dd0
SylvainLan <sylvain.lannuzel@student.ecp.fr>
2017-10-23 11:38:57 -0400

    test updated

--- test.sh
+++ test.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-#SBATCH --verbose
-#SBATCH --job-name=0_python3--test_cuda.py
-ript-validate.sh-pronoun_enfr_dev.lang1-pronoun_enfr_dev.lang2
-#SBATCH --gres=gpu:k80:1
-#SBATCH --time=01:00:00
-#SBATCH --mem=2GB
+
+#SBATCH --job-name=test_cuda
 #SBATCH --nodes=1
-#SBATCH --output=/home/ss11642/mrf_dl/slurm_%j.out
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=12GB
+#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:3
+#SBATCH --partition=k80_8
 
 module purge
-module load python3/intel/3.5.3
 module load pytorch/python3.5/0.2.0_3
-
-
-SRC=/home/ss11642/mrf_dl
-
-
-
-cd $SRC
 
 python3 test_cuda.py
