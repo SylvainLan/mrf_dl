176908ef8fc1081e4c33f9a90ae34a2402028176
SylvainLan <sylvain.lannuzel@student.ecp.fr>
2017-11-01 15:53:21 -0400

    fix typos

--- models/gflstm.py
+++ models/gflstm.py
@@ -46,7 +46,6 @@ class GFLSTM(nn.Module):
             self.add_module('h2h_o_' + str(i), nn.Linear(hidden_dim, hidden_dim))
             self.add_module('h2h_g_' + str(i), nn.Linear(steps * hidden_dim, steps))
             self.add_module('h2h_c_' + str(i), nn.Linear(steps * hidden_dim, steps * hidden_dim))
-        self.add_module('last_l', nn.Linear(rep_time * hidden_dim, out_dim))
 
         self.i2h_f = AttrProxy(self, 'i2h_f_')
         self.h2h_f = AttrProxy(self, 'h2h_f_')
@@ -58,30 +57,31 @@ class GFLSTM(nn.Module):
         self.h2h_g = AttrProxy(self, 'h2h_g_')
         self.i2h_c = AttrProxy(self, 'i2h_c_')
         self.h2h_c = AttrProxy(self, 'h2h_c_')
-        self.last_l = AttrProxy(self, 'last_l')
+        self.last_l = nn.Linear((rep_time - 1) * hidden_dim, out_dim)
 
     def forward(self, input, hidden, current):
         for t in range(self.rep_time):
             h_next = []
             c_next = []
+            input_h = input
             for l in range(self.steps):
-                f = func.sigmoid(self.i2h_f[l](input) + self.h2h_f[l](hidden[t][l]))
-                i = func.sigmoid(self.i2h_i[l](input) + self.h2h_i[l](hidden[t][l]))
-                g = func.sigmoid(self.i2h_g[l](input) + self.h2h_g[l](torch.cat(hidden[t], 1)))
+                f = func.sigmoid(self.i2h_f[l](input_h) + self.h2h_f[l](hidden[t][l]))
+                i = func.sigmoid(self.i2h_i[l](input_h) + self.h2h_i[l](hidden[t][l]))
+                g = func.sigmoid(self.i2h_g[l](input_h) + self.h2h_g[l](torch.cat(hidden[t], 1)))
                 aux = self.h2h_c[l](torch.cat(hidden[t], 1))
                 aux = aux.view(-1, self.steps, self.hidden_dim)
                 g = g.view(-1, self.steps, 1)
                 aux = aux * g
                 aux = aux.sum(1).view(-1, self.hidden_dim)
 
-                c_t = func.tanh(self.i2h_c[l](input) + aux)
+                c_t = func.tanh(self.i2h_c[l](input_h) + aux)
                 c = f * current[l] + i * c_t
-                o = func.sigmoid(self.i2h_o[l](input) + self.h2h_o[l](hidden[l]))
+                o = func.sigmoid(self.i2h_o[l](input_h) + self.h2h_o[l](hidden[t][l]))
                 h = o * c
-                input = h
+                input_h = h
                 h_next.append(h)
                 c_next.append(c)
             hidden.append(h_next)
-            current = c
-        out = F.sigmoid(self.last_l(torch.cat([h[l + 1][-1] for l in range(self.rep_time - 1)], 1)))
+            current = c_next
+        out = func.sigmoid(self.last_l(torch.cat([hidden[l + 1][-1] for l in range(self.rep_time - 1)], 1)))
         return out
